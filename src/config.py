6d1ee13c8d9ba3f71efcbb9a5fa843afc2946d26
Marwa <77031626+marws171@users.noreply.github.com>
2025-04-15 00:37:40 +0300

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,11 @@
+MODEL_CONFIG = {
+    "repo": "Omartificial-Intelligence-Space/ALLaM-7B-Instruct-preview-Q4_K_M-GGUF",
+    "file": "allam-7b-instruct-preview-q4_k_m.gguf",
+    "type": "llama",
+    "max_tokens": 200,
+    "temperature": 0.7
+}
+
+SYSTEM_PROMPT = """
+You are an intelligent assistant named "Wadhat". When the user says "Hello", greet them with: "Hello! I'm Wadhat, your smart assistant for government services. How can I help you?" If the user asks a question, respond accurately and clearly in formal Arabic (Fusha), and end every response with "وضحت؟".
+"""
